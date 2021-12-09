f7d73f0329005f29abcdb945a51cde7b1f1ee703
Ejaz <mohammadejaz978@gmail.com>
2021-12-09 11:51:54 +0530

    first commit

---
+++ card/src/Components/CardComp/Card.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import data from "./Menu";
+const Card = () => {
+  const [state, setState] = useState(data);
+
+
+  const FilterItem = (categoryItem) => {
+     const updateItem = data.filter((item) => {
+         return(
+             item.category === categoryItem
+         )
+     })
+     setState(updateItem)
+  }
+
+
+
+  return (
+    <>
+      <div className="btnList">
+        <ul>
+          <li>
+            <a className="active" href="#" onClick={()=>{FilterItem("juice")}}>Fruits</a>
+          </li>
+          <li>
+            <a href="#" onClick={()=>{FilterItem("fruits")}} >Juice</a>
+          </li>
+          <li>
+            <a href="#" onClick={()=>{FilterItem("Vegetables")}} >Vegetables</a>
+          </li>
+          <li>
+            <a href="#" onClick={()=>{ setState(data)}}>All</a>
+          </li>
+        </ul>
+      </div>
+
+      {state.map((item) => {
+        console.log(item);
+
+        return (
+          <div className="container" key={item.id}>
+            <div className="wrapper">
+              <div className="img">
+                <img src={item.img} alt={"food"} />
+              </div>
+
+              <div className="description">
+                <p>{item.description}</p>
+
+                <div className="anchor">
+                  <a href="#">Read More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Card;
---
+++ card/src/Components/CardComp/Menu.js
@@ -0,0 +1,75 @@
+const data = [
+    {
+        id:"1",
+        category : "fruits",
+        title : "", 
+        img : "https://media.istockphoto.com/photos/fruit-smoothies-picture-id173501950?b=1&k=20&m=173501950&s=170667a&w=0&h=wbqiN2cMzhr340_JUmuCZbPx1vFb2pMcX6_F5CPVhkU=",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"2",
+        category : "fruits",
+        title : "",
+         img : "https://images.unsplash.com/photo-1481671703460-040cb8a2d909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8anVpY2UlMjBwaWN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"3",
+        category : "fruits",
+        title : "",
+         img : "https://images.unsplash.com/photo-1601924287811-e34de5d17476?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGp1aWNlJTIwcGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    }
+    ,
+    {
+        id:"4",
+        category : "fruits",
+        title : "",
+         img : "https://images.unsplash.com/photo-1528556860752-2a6a19a285a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGp1aWNlJTIwcGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"5",
+        category : "juice",
+        title : "", 
+        img : "https://media.istockphoto.com/photos/set-of-summer-fruits-and-berries-in-wooden-serving-picture-id1273378551?b=1&k=20&m=1273378551&s=170667a&w=0&h=8rjG-Q-mefnIar9ydImScP7-cmVKqGi_82KSjRHMGg8=",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"6",
+        category : "juice",
+        title : "",
+         img : "https://media.istockphoto.com/photos/fresh-mixed-fruits-picture-id467652436?b=1&k=20&m=467652436&s=170667a&w=0&h=SgDVjLV5rfJ-kJ80GYcQJ4CL1R0n4LoxTYXixnSZuWs=",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"7",
+        category : "juice",
+        title : "",
+         img : "https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZnJ1aXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"8",
+        category : "juice",
+        title : "",
+         img : "https://media.istockphoto.com/photos/delicious-fruit-salad-on-a-plate-on-table-picture-id1298307574?b=1&k=20&m=1298307574&s=170667a&w=0&h=d1VWlyALXWare4uxuTxuK5he0Dtp5dRzpC2Sy2gOKyk=",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"9",
+        category : "Vegetables",
+        title : "", 
+        img : "https://images.unsplash.com/photo-1598030304671-5aa1d6f21128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+    {
+        id:"10",
+        category : "Vegetables",
+        title : "",
+         img : "https://media.istockphoto.com/photos/food-background-with-assortment-of-fresh-organic-vegetables-picture-id1203599923?b=1&k=20&m=1203599923&s=170667a&w=0&h=SxSyRNqrW8JQsyh6gCktCwcc16yX-Ymo2-I7NNTRtcA=",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, praesentium!"
+    },
+]
+
+export default data
